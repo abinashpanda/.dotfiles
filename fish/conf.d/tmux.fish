2762d4b6b01cdbc12057b12723c63bb56fd6be4a
Abinash Panda <abinash@prodios.com>
2020-08-02 13:17:00 +0530

    Supress fish greeting message

--- fish/conf.d/tmux.fish
+++ fish/conf.d/tmux.fish
@@ -1,3 +1,6 @@
+set LANG en_US.UTF-8
+set LC_CTYPE en_US.UTF-8
+
 if not set -q TMUX
     set -g TMUX tmux new-session -d -s base
     eval $TMUX
--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,4 @@
+set fish_greeting
+
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
