0afe1b909ecf132c3ef9432bdb459834b77b6458
Abinash Panda <abinash@prodios.com>
2024-03-21 12:49:00 +0530

    fix(wsl): remove unused configs

--- windows-wsl/connect_wsl.ps1
+++
@@ -1,9 +0,0 @@
-# Enable access to metro bundler from Windows
-# https://gist.github.com/bergmannjg/461958db03c6ae41a66d264ae6504ade#enable-access-to-metro-bundler-from-windows
-# Run the script as admin
-
-iex "netsh interface portproxy delete v4tov4 listenport=8081 listenaddress=127.0.0.1" | out-null;
-$WSL_CLIENT = bash.exe -c "ifconfig eth0 | grep 'inet '";
-$WSL_CLIENT -match '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}';
-$WSL_CLIENT = $matches[0];
-iex "netsh interface portproxy add v4tov4 listenport=8081 listenaddress=127.0.0.1 connectport=8081 connectaddress=$WSL_CLIENT"
--- windows-wsl/start_adb.ps1
+++
@@ -1,5 +0,0 @@
-# Start the adb server
-# https://gist.github.com/bergmannjg/461958db03c6ae41a66d264ae6504ade#start-adb-server-in-windows
-
-adb kill-server
-adb -a nodaemon server start
