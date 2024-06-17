48a916eb9ab1d4781688ad485df4901950adc3b3
Abinash Panda <abinash@prodios.com>
2024-06-17 12:07:21 +0530

    feat(nvim): add toggle keymap to nvim-tree

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -9,3 +9,6 @@ map("i", "jk", "<ESC>")
 
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 map("n", "<leader>tt", '<CMD>lua require("FTerm").toggle()<CR>')
+
+local nvim_tree_api = require "nvim-tree.api"
+map("n", "<leader>e", nvim_tree_api.tree.toggle)
